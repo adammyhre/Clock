497277b80aa49db9eb68a0899ba071d3b552dffc
Adam Myhre <adam@undergalaxie.com>
2019-06-22 11:45:14 -0700

    first commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+	public Transform hoursTransform, minutesTransform, secondsTransform;
+	public bool continuous = true;
+	const float 
+		degreesPerHour = 30f,
+		degreesPerMinute = 6f,
+		degreesPerSecond = 6f;
+
+		
+	void Update () {
+		if (continuous) {
+			UpdateContinuous();
+		}
+		else {
+			UpdateDiscrete();
+		}
+	}
+
+	void UpdateDiscrete () {
+		DateTime time = DateTime.Now;
+		hoursTransform.localRotation =
+			Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+		minutesTransform.localRotation =
+			Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+		secondsTransform.localRotation =
+			Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+	}
+
+	void UpdateContinuous () {
+		TimeSpan time = DateTime.Now.TimeOfDay;
+		hoursTransform.localRotation =
+			Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+		minutesTransform.localRotation =
+			Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+		secondsTransform.localRotation =
+			Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+	}
+
+}
